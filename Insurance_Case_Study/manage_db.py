ba26de64fcc6669e1ef692d0300fb755c9f60598
Peter Lugalia <peter.lugalia@bbraun.com>
2022-09-28 22:25:15 +0200

    refresh data base

--- Insurance_Case_Study/manage_db.py
+++ Insurance_Case_Study/manage_db.py
@@ -17,13 +17,13 @@ def view_data():
 
 
 def track_review():
-    c.execute('CREATE TABLE IF NOT EXISTS review (Foreign_worker TEXT, Gender TEXT, Status TEXT, Credit_History TEXT, Saving_acccount_bonds TEXT,Installment_Rate TEXT,  Debtors_Guarantors TEXT, Property TEXT, Age INT, Other_Installment_Plans TEXT, Housing TEXT,Duration INT,Installment_Rate_Income INT,Credit_Amount)')
+    c.execute('CREATE TABLE IF NOT EXISTS review (Foreign_worker TEXT, Gender VARCHAR(256), Status TEXT, Credit_History TEXT, Saving_acccount_bonds TEXT,Installment_Rate TEXT,  Debtors_Guarantors TEXT, Property TEXT, Age INT, Other_Installment_Plans TEXT, Housing TEXT,Duration INT,Installment_Rate_Income INT,Credit_Amount)')
     conn.commit()
 
 def add_prediction(Foreign_worker, Gender, Status,  Credit_History, Saving_acccount_bonds,Installment_Rate,  Debtors_Guarantors, Property,Age, Other_Installment_Plans, Housing, Duration,Installment_Rate_Income,Credit_Amount):
-        c.execute('INSERT INTO review (Foreign_worker, Gender, Status,  Credit_History, Saving_acccount_bonds,Installment_Rate,  Debtors_Guarantors, Property,Age, Other_Installment_Plans, Housing, Duration,Installment_Rate_Income, Credit_Amount'
-        ') VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)', (Foreign_worker, Gender, Status,  Credit_History, Saving_acccount_bonds,Installment_Rate,  Debtors_Guarantors, Property,Age, Other_Installment_Plans, Housing, Duration,Installment_Rate_Income,
-        Credit_Amount))
+    c.execute('INSERT INTO review (Foreign_worker, Gender, Status,  Credit_History, Saving_acccount_bonds,Installment_Rate,  Debtors_Guarantors, Property,Age, Other_Installment_Plans, Housing, Duration,Installment_Rate_Income, Credit_Amount)'
+     'VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)', (Foreign_worker, Gender, Status,  Credit_History, Saving_acccount_bonds,Installment_Rate,  Debtors_Guarantors, Property,Age, Other_Installment_Plans, Housing, Duration,Installment_Rate_Income, Credit_Amount))
+    conn.commit()
 
 def view_all_data():
     c.execute('SELECT * FROM review')
